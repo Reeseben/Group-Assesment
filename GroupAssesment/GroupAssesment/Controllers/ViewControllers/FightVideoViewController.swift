2b84ccf0ddbb3b40c57e263d129fb2961ae0a755
Israel Romero <israelromero@Israels-MacBook-Pro.local>
2021-09-13 13:26:13 -0600

    added new Mp4 and updated the AvPlayer code

--- GroupAssesment/GroupAssesment/Controllers/ViewControllers/FightVideoViewController.swift
+++ GroupAssesment/GroupAssesment/Controllers/ViewControllers/FightVideoViewController.swift
@@ -14,7 +14,7 @@ class FightVideoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "challenger", ofType: "mp4")!))
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "fight", ofType: "mp4")!))
         
         let layer = AVPlayerLayer(player:  player)
         layer.frame = view.bounds
